e243115f791cc033577589de2bd38237867c66c0
Fadi Nassri <fadinassri@gmail.com>
2022-01-09 16:21:17 -0400

    Adding Simple Test Case NUnit

--- TestTestExample/UnitTest1.cs
+++ TestTestExample/UnitTest1.cs
@@ -1,4 +1,8 @@
+using Models;
 using NUnit.Framework;
+using TestExample;
+using System.Linq;
+using Moq;
 
 namespace TestTestExample
 {
@@ -7,12 +11,26 @@ namespace TestTestExample
         [SetUp]
         public void Setup()
         {
+            svcMoq = new MoqBookingService();
         }
+        private IBookService svcMoq;
 
         [Test]
-        public void Test1()
+        public void checkOverlap_CallWithOverLap_ReturnOverlappedRecord()
         {
-            Assert.Pass();
+            //var svcMoq = new Mock<IBookService>();
+            //svcMoq.Setup(x => x.GetAllBooking()).Returns(new System.Collections.Generic.List<BookIngDetail>());
+
+            BookIngDetail b = new BookIngDetail();
+            b.BookStatus = true;
+            b.Destination = "Toronto";
+            BookingHelper bkhg = new BookingHelper(svcMoq);
+            var result = bkhg.checkOverlap(b);
+            Assert.That(result.Contains("Toronto"));
         }
+
+
+
+
     }
 }
