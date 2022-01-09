9756a0e8eb2b58294fa50f0cfe7f539517f5e5f5
Fadi Nassri <fadinassri@gmail.com>
2022-01-09 16:52:14 -0400

    Refactor

--- TestTestExample/UnitTest1.cs
+++ TestTestExample/UnitTest1.cs
@@ -11,26 +11,26 @@ namespace TestTestExample
         [SetUp]
         public void Setup()
         {
-           // svcMoq = new MoqBookingService();
-        }
-        private IBookService svcMoq;
-
-        [Test]
-        public void checkOverlap_CallWithOverLap_ReturnOverlappedRecord()
-        {
-            var svcMoq = new Mock<IBookService>();
+            _svcMoq = new Mock<IBookService>();
             System.Collections.Generic.List<BookIngDetail> value = new System.Collections.Generic.List<BookIngDetail>() {
             new BookIngDetail(){Id = 1, BookStatus=true, Destination="Toronto"},
             new BookIngDetail(){Id = 2, BookStatus=true, Destination="Jamaika"},
             new BookIngDetail(){Id = 3, BookStatus=true, Destination="BC"},
             };
+            _svcMoq.Setup(x => x.GetAllBooking()).Returns(value);
 
-            svcMoq.Setup(x => x.GetAllBooking()).Returns(value);
+        }
+        private Mock<IBookService> _svcMoq;
+
+        [Test]
+        public void checkOverlap_CallWithOverLap_ReturnOverlappedRecord()
+        {
+
 
             BookIngDetail b = new BookIngDetail();
             b.BookStatus = true;
             b.Destination = "Toronto";
-            BookingHelper bkhg = new BookingHelper(svcMoq.Object);
+            BookingHelper bkhg = new BookingHelper(_svcMoq.Object);
             var result = bkhg.checkOverlap(b);
             Assert.That(result.Contains("Toronto"));
         }
